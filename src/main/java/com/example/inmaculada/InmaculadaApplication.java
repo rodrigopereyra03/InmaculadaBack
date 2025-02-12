45f15e0fcde3144d031eb9ac04b7dc2c83449387
Rodrigo Pereyra <rodrigopereyra02@gmail.com>
2025-02-12 18:43:17 -0300

    git

---
+++ src/main/java/com/example/inmaculada/InmaculadaApplication.java
@@ -0,0 +1,13 @@
+package com.example.inmaculada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InmaculadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InmaculadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/inmaculada/InmaculadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inmaculada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InmaculadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
